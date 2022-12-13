430e0a796a3205a7649713d4c15614b85ec904c6
Glitch0320 <100811040+Glitch0320@users.noreply.github.com>
2022-12-13 16:15:28 -0600

    updating package.json...

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const path = require('path')
+require('dotenv').config()
 const db = require('./config/connection')
 const routes = require('./routes')
 
@@ -15,6 +16,10 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 app.use(routes)
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+})
 
 db.once('open', () => {
   app.listen(PORT, () => console.log(`Now listening on localhost: ${PORT}`))

e491f0a5b6d781290f2cf918317e53f7a4ceb745
Glitch0320 <100811040+Glitch0320@users.noreply.github.com>
2022-12-15 20:15:56 -0600

    margin to saved paths

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -113,7 +113,7 @@ const Profile = () => {
                   border: '.25rem solid #2cff0f',
                   borderRadius: '.5rem',
                   padding: '.5rem',
-                  marginTop: '1.5rem'
+                  margin: '1.5rem 1rem 0 1rem'
                 }}
               >
                 {
